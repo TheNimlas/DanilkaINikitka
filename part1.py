e480e13ab1b670ee864b30578988afe5bc478726
Nikita <nimals3392@mail.ru>
2020-02-15 13:51:02 +0300

    Task №2

--- part1.py
+++ part1.py
@@ -7,22 +7,45 @@ from PyQt5.QtWidgets import QWidget, QApplication
 
 class Window(QWidget, Ui_maps):
     def __init__(self):
+        print(1)
         super().__init__()
         self.setupUi(self)
-        map_request = "https://static-maps.yandex.ru/1.x/?ll=133.795384,-25.694768&spn=20.700,20.700&l=sat"
-        response = requests.get(map_request)
+        self.map_request = "https://static-maps.yandex.ru/1.x/?ll=37.617635,55.755814&" \
+                           "spn={},{}&l=sat"
+        self.i = 0.005
+        self.response = requests.get(self.map_request.format(self.i, self.i))
 
-        if not response:
+        if not self.response:
             print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
+            print(self.map_request)
+            print("Http статус:", self.response.status_code, "(", self.response.reason, ")")
             sys.exit(1)
 
         map_file = "map.jpg"
         with open(map_file, "wb") as file:
-            file.write(response.content)
+            file.write(self.response.content)
         self.map_pic = QPixmap(map_file)
         self.map.setPixmap(self.map_pic)
+
+    def keyPressEvent(self, event):
+        if event.key() == 16777238:
+            if self.i > 0:
+                self.i -= 0.005
+                self.response = requests.get(self.map_request.format(self.i, self.i))
+                map_file = "map.jpg"
+                with open(map_file, "wb") as file:
+                    file.write(self.response.content)
+                self.map_pic = QPixmap(map_file)
+                self.map.setPixmap(self.map_pic)
+        elif event.key() == 16777239:
+            if 0 <= self.i < 75:
+                self.i += 0.005
+                self.response = requests.get(self.map_request.format(self.i, self.i))
+                map_file = "map.jpg"
+                with open(map_file, "wb") as file:
+                    file.write(self.response.content)
+                self.map_pic = QPixmap(map_file)
+                self.map.setPixmap(self.map_pic)
 
 
 if __name__ == '__main__':
@@ -30,3 +53,5 @@ if __name__ == '__main__':
     ex = Window()
     ex.show()
     sys.exit(app.exec_())
+
+
