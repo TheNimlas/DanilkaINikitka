6121f5b4ac8e7c501dce922479a0b00d82153970
danilus-lab <daniil.novsky@yandex.ru>
2020-02-16 11:22:16 +0300

    task5 maybe ready

--- map_window.py
+++ map_window.py
@@ -13,11 +13,94 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_maps(object):
     def setupUi(self, maps):
         maps.setObjectName("maps")
-        maps.resize(668, 555)
+        maps.resize(863, 519)
         self.map = QtWidgets.QLabel(maps)
-        self.map.setGeometry(QtCore.QRect(10, 10, 641, 531))
+        self.map.setGeometry(QtCore.QRect(10, 0, 651, 481))
+        self.map.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.map.setText("")
         self.map.setObjectName("map")
+        self.verticalLayoutWidget = QtWidgets.QWidget(maps)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(640, 20, 171, 128))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.ma_format = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        self.ma_format.setFont(font)
+        self.ma_format.setStyleSheet("QPushButton{\n"
+"            background-color: rgb(0, 0, 0);\n"
+"            color: #fcfaff;\n"
+"             border-radius: 17px transparent;\n"
+"           border-bottom: 3px transparent;\n"
+"            border-right: 2px transparent;\n"
+"            border-left: 2px transparent;}\n"
+"            QPushButton:hover{\n"
+"            background-color: rgb(255,0,0);} \n"
+"            QPushButton:pressed  {\n"
+"            background-color: rgb(232,95,76); } ")
+        self.ma_format.setObjectName("ma_format")
+        self.verticalLayout.addWidget(self.ma_format)
+        self.sat_format = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        self.sat_format.setFont(font)
+        self.sat_format.setStyleSheet("QPushButton{\n"
+"            background-color: rgb(0, 0, 0);\n"
+"            color: #fcfaff;\n"
+"             border-radius: 17px transparent;\n"
+"           border-bottom: 3px transparent;\n"
+"            border-right: 2px transparent;\n"
+"            border-left: 2px transparent;}\n"
+"            QPushButton:hover{\n"
+"            background-color: rgb(255,0,0);} \n"
+"            QPushButton:pressed  {\n"
+"            background-color: rgb(232,95,76); } ")
+        self.sat_format.setObjectName("sat_format")
+        self.verticalLayout.addWidget(self.sat_format)
+        self.gibrid_format = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        self.gibrid_format.setFont(font)
+        self.gibrid_format.setStyleSheet("QPushButton{\n"
+"            background-color: rgb(0, 0, 0);\n"
+"            color: #fcfaff;\n"
+"             border-radius: 17px transparent;\n"
+"           border-bottom: 3px transparent;\n"
+"            border-right: 2px transparent;\n"
+"            border-left: 2px transparent;}\n"
+"            QPushButton:hover{\n"
+"            background-color: rgb(255,0,0);} \n"
+"            QPushButton:pressed  {\n"
+"            background-color: rgb(232,95,76); } ")
+        self.gibrid_format.setObjectName("gibrid_format")
+        self.verticalLayout.addWidget(self.gibrid_format)
+        self.find_line = QtWidgets.QLineEdit(maps)
+        self.find_line.setGeometry(QtCore.QRect(670, 420, 161, 41))
+        self.find_line.setFocusPolicy(QtCore.Qt.WheelFocus)
+        self.find_line.setObjectName("find_line")
+        self.find_button = QtWidgets.QPushButton(maps)
+        self.find_button.setGeometry(QtCore.QRect(690, 472, 111, 41))
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        self.find_button.setFont(font)
+        self.find_button.setStyleSheet("QPushButton{\n"
+"            background-color: rgb(0, 0, 0);\n"
+"            color: #fcfaff;\n"
+"             border-radius: 17px transparent;\n"
+"           border-bottom: 3px transparent;\n"
+"            border-right: 2px transparent;\n"
+"            border-left: 2px transparent;}\n"
+"            QPushButton:hover{\n"
+"            background-color: rgb(255,0,0);} \n"
+"            QPushButton:pressed  {\n"
+"            background-color: rgb(232,95,76); } ")
+        self.find_button.setObjectName("find_button")
+        self.verticalLayoutWidget.raise_()
+        self.map.raise_()
+        self.find_line.raise_()
+        self.find_button.raise_()
 
         self.retranslateUi(maps)
         QtCore.QMetaObject.connectSlotsByName(maps)
@@ -25,3 +108,7 @@ class Ui_maps(object):
     def retranslateUi(self, maps):
         _translate = QtCore.QCoreApplication.translate
         maps.setWindowTitle(_translate("maps", "nimals_and_danilus_maps"))
+        self.ma_format.setText(_translate("maps", "Формат карты"))
+        self.sat_format.setText(_translate("maps", "Формат спутника"))
+        self.gibrid_format.setText(_translate("maps", "Hybrid"))
+        self.find_button.setText(_translate("maps", "Find"))
--- part1.py
+++ part1.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore
 from map_window import Ui_maps
 from PyQt5.QtWidgets import QWidget, QApplication
 
@@ -9,20 +10,128 @@ class Window(QWidget, Ui_maps):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        map_request = "https://static-maps.yandex.ru/1.x/?ll=133.795384,-25.694768&spn=20.700,20.700&l=sat"
-        response = requests.get(map_request)
-
-        if not response:
+        self.map_request = "https://static-maps.yandex.ru/1.x/?ll={},{}&" \
+                           "spn={},{}&l={}"  # &pt=37.617635,55.755814
+        self.first_coord = 37.617635
+        self.second_coord = 55.755814
+        self.i = 0.005
+        self.format = 'jpg'
+        self.style = 'sat'
+        self.params = {'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
+                       'first_coord': self.first_coord,
+                       'second_coord': self.second_coord,
+                       'pt[0]': self.first_coord,
+                       'pt[1]': self.second_coord}
+        self.find_button.setText(chr(128269))
+        self.ma_format.setText(chr(128506))
+        self.sat_format.setText(chr(128752))
+        self.response = requests.get(
+            self.map_request.format(self.first_coord, self.second_coord, self.i, self.i,
+                                    self.style))
+        self.make_a_mark = False
+        if not self.response:
             print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
+            print(self.map_request)
+            print("Http статус:", self.response.status_code, "(", self.response.reason, ")")
             sys.exit(1)
-
         map_file = "map.jpg"
         with open(map_file, "wb") as file:
-            file.write(response.content)
+            file.write(self.response.content)
         self.map_pic = QPixmap(map_file)
         self.map.setPixmap(self.map_pic)
+        self.sat_format.clicked.connect(self.change_format)
+        self.ma_format.clicked.connect(self.change_format)
+        self.gibrid_format.clicked.connect(self.change_format)
+        self.find_button.clicked.connect(self.find)
+
+    def find(self):
+        address = self.find_line.text()
+        try:
+            self.make_a_mark = True
+            print(self.params)
+            geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-" \
+                               "98ba-98533de7710b&geocode={}&format=json".format(address)
+            response = requests.get(geocoder_request)
+            json_response = response.json()
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0][
+                "GeoObject"]
+            toponym_coodrinates = toponym["Point"]["pos"]
+            self.params['first_coord'] = float(toponym_coodrinates.split()[0])
+            self.params['second_coord'] = float(toponym_coodrinates.split()[1])
+            self.first_coord = float(toponym_coodrinates.split()[0])
+            self.second_coord = float(toponym_coodrinates.split()[1])
+            self.params['pt[0]'] = float(toponym_coodrinates.split()[0])
+            self.params['pt[1]'] = float(toponym_coodrinates.split()[1])
+            self.place_a_map()
+        except Exception as e:
+            print(e.__class__.__name__)
+            self.map_pic = QPixmap('error.jpg')
+            self.map.setPixmap(self.map_pic)
+
+    def place_a_map(self):
+        map_file = "map.jpg"
+        if self.make_a_mark:
+            self.map_request = "https://static-maps.yandex.ru/1.x/?ll={},{}&" \
+                               "spn={},{}&l={}&pt={},{}"
+            self.response = requests.get(
+                self.map_request.format(self.params['first_coord'], self.params['second_coord'],
+                                        self.i, self.i, self.style,
+                                        self.params['pt[0]'], self.params['pt[1]']))
+        else:
+            self.response = requests.get(
+                self.map_request.format(self.params['first_coord'], self.params['second_coord'],
+                                        self.i, self.i, self.style))
+        if self.format == 'jpg':
+            map_file = "map.jpg"
+        elif self.format == 'png':
+            map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(self.response.content)
+        self.map_pic = QPixmap(map_file)
+        self.map.setPixmap(self.map_pic)
+
+    def change_format(self):
+        if self.sender() == self.sat_format:
+            self.format = 'jpg'
+            self.style = 'sat'
+        elif self.sender() == self.ma_format:
+            self.format = 'png'
+            self.style = 'map'
+        elif self.sender() == self.gibrid_format:
+            self.format = 'png'
+            self.style = 'skl'
+        self.place_a_map()
+
+    def keyPressEvent(self, event):
+        try:
+            if event.key() == 16777238:
+                if self.i > 0:
+                    self.i -= 0.005
+                    self.place_a_map()
+            elif event.key() == 16777239:
+                if 0 <= self.i < 75:
+                    self.i += 0.005
+                    self.place_a_map()
+            elif event.key() == QtCore.Qt.Key_Up:
+                print(1)
+                print(self.params)
+                if self.params['second_coord'] < self.second_coord + 0.001:
+                    self.params['second_coord'] += 0.0002
+                    self.place_a_map()
+            elif event.key() == QtCore.Qt.Key_Down:
+                if self.params['second_coord'] > self.second_coord - 0.001:
+                    self.params['second_coord'] -= 0.0002
+                    self.place_a_map()
+            elif event.key() == QtCore.Qt.Key_Right:
+                if self.params['first_coord'] < self.first_coord + 0.002:
+                    self.params['first_coord'] += 0.0002
+                    self.place_a_map()
+            elif event.key() == QtCore.Qt.Key_Left:
+                if self.params['first_coord'] > self.first_coord - 0.002:
+                    self.params['first_coord'] -= 0.0002
+                    self.place_a_map()
+        except Exception as e:
+            print(e.__class__.__name__)
 
 
 if __name__ == '__main__':
