3b7b7300bc21824fae1022951189c26457ebfd92
danilus-lab <daniil.novsky@yandex.ru>
2020-02-16 09:17:53 +0300

    task4

--- map_window.py
+++ map_window.py
@@ -2,22 +2,29 @@
 
 # Form implementation generated from reading ui file 'map_window.ui'
 #
-# Created by: PyQt5 UI code generator 5.13.0
+# Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_maps(object):
     def setupUi(self, maps):
         maps.setObjectName("maps")
-        maps.resize(668, 555)
+        maps.resize(863, 519)
         self.map = QtWidgets.QLabel(maps)
-        self.map.setGeometry(QtCore.QRect(10, 10, 641, 531))
+        self.map.setGeometry(QtCore.QRect(10, 10, 651, 481))
         self.map.setText("")
         self.map.setObjectName("map")
+        self.ma_format = QtWidgets.QPushButton(maps)
+        self.ma_format.setGeometry(QtCore.QRect(720, 40, 111, 31))
+        self.ma_format.setObjectName("ma_format")
+        self.sat_format = QtWidgets.QPushButton(maps)
+        self.sat_format.setGeometry(QtCore.QRect(720, 80, 111, 31))
+        self.sat_format.setObjectName("sat_format")
+        self.gibrid_format = QtWidgets.QPushButton(maps)
+        self.gibrid_format.setGeometry(QtCore.QRect(720, 120, 111, 31))
+        self.gibrid_format.setObjectName("gibrid_format")
 
         self.retranslateUi(maps)
         QtCore.QMetaObject.connectSlotsByName(maps)
@@ -25,3 +32,7 @@ class Ui_maps(object):
     def retranslateUi(self, maps):
         _translate = QtCore.QCoreApplication.translate
         maps.setWindowTitle(_translate("maps", "nimals_and_danilus_maps"))
+        self.ma_format.setText(_translate("maps", "формат карты"))
+        self.sat_format.setText(_translate("maps", "формат спутника"))
+        self.gibrid_format.setText(_translate("maps", "гибридный формат"))
+
--- part1.py
+++ part1.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore
 from map_window import Ui_maps
 from PyQt5.QtWidgets import QWidget, QApplication
 
@@ -9,20 +10,82 @@ class Window(QWidget, Ui_maps):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        map_request = "https://static-maps.yandex.ru/1.x/?ll=133.795384,-25.694768&spn=20.700,20.700&l=sat"
-        response = requests.get(map_request)
+        self.map_request = "https://static-maps.yandex.ru/1.x/?ll={},{}&" \
+                           "spn={},{}&l={}"
+        self.first_coord = 37.617635
+        self.second_coord = 55.755814
+        self.i = 0.005
+        self.format = 'jpg'
+        self.style = 'sat'
+        self.response = requests.get(
+            self.map_request.format(self.first_coord, self.second_coord, self.i, self.i,
+                                    self.style))
 
-        if not response:
+        if not self.response:
             print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
+            print(self.map_request)
+            print("Http статус:", self.response.status_code, "(", self.response.reason, ")")
             sys.exit(1)
-
         map_file = "map.jpg"
         with open(map_file, "wb") as file:
-            file.write(response.content)
+            file.write(self.response.content)
         self.map_pic = QPixmap(map_file)
         self.map.setPixmap(self.map_pic)
+        self.sat_format.clicked.connect(self.change_format)
+        self.ma_format.clicked.connect(self.change_format)
+        self.gibrid_format.clicked.connect(self.change_format)
+
+    def place_a_map(self):
+        map_file = "map.jpg"
+        self.response = requests.get(
+            self.map_request.format(self.first_coord, self.second_coord, self.i, self.i,
+                                    self.style))
+        if self.format == 'jpg':
+            map_file = "map.jpg"
+        elif self.format == 'png':
+            map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(self.response.content)
+        self.map_pic = QPixmap(map_file)
+        self.map.setPixmap(self.map_pic)
+
+    def change_format(self):
+        if self.sender() == self.sat_format:
+            self.format = 'jpg'
+            self.style = 'sat'
+        elif self.sender() == self.ma_format:
+            self.format = 'png'
+            self.style = 'map'
+        elif self.sender() == self.gibrid_format:
+            self.format = 'png'
+            self.style = 'skl'
+        self.place_a_map()
+
+    def keyPressEvent(self, event):
+        if event.key() == 16777238:
+            if self.i > 0:
+                self.i -= 0.005
+                self.place_a_map()
+        elif event.key() == 16777239:
+            if 0 <= self.i < 75:
+                self.i += 0.005
+                self.place_a_map()
+        elif event.key() == QtCore.Qt.Key_Up:
+            if self.second_coord < 55.756814:
+                self.second_coord += 0.0002
+                self.place_a_map()
+        elif event.key() == QtCore.Qt.Key_Down:
+            if self.second_coord > 55.754814:
+                self.second_coord -= 0.0002
+                self.place_a_map()
+        elif event.key() == QtCore.Qt.Key_Right:
+            if self.first_coord < 37.619635:
+                self.first_coord += 0.0002
+                self.place_a_map()
+        elif event.key() == QtCore.Qt.Key_Left:
+            if self.first_coord > 37.615635:
+                self.first_coord -= 0.0002
+                self.place_a_map()
 
 
 if __name__ == '__main__':
